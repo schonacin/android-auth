50f0a4bfe1cf4c973f145147bf59754bac4a4f01
Jonathan Wilke <jonathan.wilke@student.hpi.uni-potsdam.de>
2019-11-13 17:36:51 +0100

    fixed example test in app folder with naming issues

--- app/src/androidTest/java/com/blue_unicorn/android_auth/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/blue_unicorn/android_auth/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.android_auth", appContext.getPackageName());
+        assertEquals("com.blue_unicorn.android_auth", appContext.getPackageName());
     }
 }
