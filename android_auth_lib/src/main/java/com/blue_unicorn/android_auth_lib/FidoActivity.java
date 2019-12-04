ddef5e0bd46e777ccbb0f056c73854aeddb7a9e3
Jonathan Wilke <jonathan.wilke@student.hpi.uni-potsdam.de>
2019-12-04 15:57:38 +0100

    added Activity Interface

--- android_auth_lib/src/main/java/com/blue_unicorn/android_auth_lib/FidoActivity.java
+++ android_auth_lib/src/main/java/com/blue_unicorn/android_auth_lib/FidoActivity.java
@@ -1,58 +1,23 @@
 package com.blue_unicorn.android_auth_lib;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
 import android.content.ServiceConnection;
-import android.os.Bundle;
-import android.os.IBinder;
 
-public abstract class FidoActivity extends android.app.Activity{
-    FidoService mFidoService;
+public interface FidoActivity {
+
     boolean mBound = false;
 
-    public void onStart() {
-        super.onStart();
-    }
+    FidoService mFidoService = null;
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    ServiceConnection connection = null;
 
-    // probably not protected in the future
-    protected void startService() {
-        //bind to FidoService
-        Intent intent = new Intent(this, FidoService.class);
-        bindService(intent, connection, Context.BIND_AUTO_CREATE);
-    }
+    void onStart();
 
-    protected void stopService() {
-        //unbind from FidoService
-        unbindService(connection);
-        mBound = false;
-    }
+    void onCreate();
 
-    protected void updateProcess(RequestObject request) {
-        // call method from FidoService only when bound
-        if (mBound) {
-            mFidoService.updateProcess(request);
-        }
-    }
+    void startService();
 
-    /** Defines callbacks for service binding */
-    private ServiceConnection connection = new ServiceConnection() {
+    void stopService();
 
-        @Override
-        public void onServiceConnected(ComponentName className, IBinder service) {
-            FidoService.LocalBinder binder = (FidoService.LocalBinder) service;
-            mFidoService = binder.getService();
-            mBound = true;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            mBound = false;
-        }
-    };
+    void updateProcess(RequestObject object);
 
 }
