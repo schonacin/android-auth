5412c89bf3d1b1e00491220e85880daad1bc466a
Jonathan Wilke <jonathan.wilke@student.hpi.uni-potsdam.de>
2019-12-19 16:06:53 +0100

    added tests for getAssertion

--- android_auth_lib/src/main/java/com/blue_unicorn/android_auth_lib/fido/PublicKeyCredentialDescriptor.java
+++ android_auth_lib/src/main/java/com/blue_unicorn/android_auth_lib/fido/PublicKeyCredentialDescriptor.java
@@ -1,9 +1,17 @@
 package com.blue_unicorn.android_auth_lib.fido;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
 public class PublicKeyCredentialDescriptor {
+
+    @SerializedName("type")
     public String type;
+
+    @SerializedName("id")
     public byte[] id;
-    public List<String> trnasports;
+
+    @SerializedName("transports")
+    public List<String> transports;
 }
