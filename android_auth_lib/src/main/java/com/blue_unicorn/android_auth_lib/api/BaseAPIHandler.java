2e2f16780b5bdd3ec9867643abe93e1db740bf04
Jonathan Wilke <jonathan.wilke@student.hpi.uni-potsdam.de>
2020-02-06 14:55:28 +0100

    removed unnecessary exception

--- android_auth_lib/src/main/java/com/blue_unicorn/android_auth_lib/api/BaseAPIHandler.java
+++ android_auth_lib/src/main/java/com/blue_unicorn/android_auth_lib/api/BaseAPIHandler.java
@@ -2,9 +2,9 @@ package com.blue_unicorn.android_auth_lib.api;
 
 import android.content.Context;
 
+import com.blue_unicorn.android_auth_lib.AuthLibException;
 import com.blue_unicorn.android_auth_lib.api.authenticator.AuthenticatorAPI;
 import com.blue_unicorn.android_auth_lib.api.authenticator.BaseAuthenticatorAPI;
-import com.blue_unicorn.android_auth_lib.exceptions.InvalidRequestObjectException;
 import com.blue_unicorn.android_auth_lib.fido.FidoObject;
 import com.blue_unicorn.android_auth_lib.fido.GetAssertionRequest;
 import com.blue_unicorn.android_auth_lib.fido.GetInfoRequest;
@@ -33,7 +33,7 @@ public class BaseAPIHandler implements APIHandler {
             } else if (request instanceof GetInfoRequest) {
                 return api.getInfo((GetInfoRequest) request);
             } else {
-                return Single.error(new InvalidRequestObjectException());
+                return Single.error(new AuthLibException("invalid request object for calling api"));
             }
         });
     }
@@ -47,7 +47,7 @@ public class BaseAPIHandler implements APIHandler {
             } else if (request instanceof GetAssertionRequest) {
                 return api.getInternalAssertion((GetAssertionRequest) request);
             } else {
-                return Single.error(new InvalidRequestObjectException());
+                return Single.error(new AuthLibException("invalid request object for updating api"));
             }
         });
     }
--- android_auth_lib/src/main/java/com/blue_unicorn/android_auth_lib/exceptions/InvalidRequestObjectException.java
+++
@@ -1,4 +0,0 @@
-package com.blue_unicorn.android_auth_lib.exceptions;
-
-public class InvalidRequestObjectException extends Exception {
-}
