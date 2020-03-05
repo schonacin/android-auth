629dbd708902ee83734adfc5ee62893824710584
Niklas van Dornick <Niklas.Dornick@student.hpi.uni-potsdam.de>
2020-03-05 11:53:21 +0100

    added comments

--- android_auth_lib/src/main/java/com/blue_unicorn/android_auth_lib/ctap2/transport_specific_bindings/ble/exceptions/BleException.java
+++ android_auth_lib/src/main/java/com/blue_unicorn/android_auth_lib/ctap2/transport_specific_bindings/ble/exceptions/BleException.java
@@ -1,5 +1,8 @@
 package com.blue_unicorn.android_auth_lib.ctap2.transport_specific_bindings.ble.exceptions;
 
+/*
+ * Subclasses represent one of the errors described in <a href="https://fidoalliance.org/specs/fido-v2.0-ps-20190130/fido-client-to-authenticator-protocol-v2.0-ps-20190130.html#ble-constants"> each.
+ */
 public abstract class BleException extends Exception {
 
     private byte errorCode;
