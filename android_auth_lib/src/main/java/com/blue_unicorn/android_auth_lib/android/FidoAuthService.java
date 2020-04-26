a0628e7d9a46ee7df34c4aa433afd9f29f4835d9
Niklas van Dornick <Niklas.Dornick@student.hpi.uni-potsdam.de>
2020-04-26 20:05:35 +0200

    cleaned up some code, ble stuff is now handled by a separate service

--- android_auth_lib/src/main/java/com/blue_unicorn/android_auth_lib/android/FidoAuthService.java
+++ android_auth_lib/src/main/java/com/blue_unicorn/android_auth_lib/android/FidoAuthService.java
@@ -8,7 +8,33 @@ import android.content.pm.PackageManager;
 import android.os.IBinder;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.blue_unicorn.android_auth_lib.ctap2.transport_specific_bindings.ble.gatt.FidoGattProfile;
+import com.nexenio.rxandroidbleserver.RxBleServer;
+
+import java.util.UUID;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import timber.log.Timber;
+
 public class FidoAuthService extends Service {
+
+    private final CompositeDisposable fidoAuthServiceDisposable = new CompositeDisposable();
+    private FidoGattProfile fidoGattProfile;
+    private RxBleServer bleServer;
+    private Disposable provideAndAdvertiseServicesDisposable;
+    private Disposable advertiseServicesDisposable;
+    //private Disposable updateValueDisposable;
+
+    private MutableLiveData<Boolean> isProvidingAndAdvertisingServices = new MutableLiveData<>();
+    private MutableLiveData<Boolean> isAdvertisingService = new MutableLiveData<>();
+    private MutableLiveData<Throwable> errors = new MutableLiveData<>();
 
     private final IBinder mBinder = new FidoAuthServiceBinder(this);
 
@@ -26,17 +52,23 @@ public class FidoAuthService extends Service {
             return null;
         }
 
+        fidoGattProfile = new FidoGattProfile(this);
+        bleServer = fidoGattProfile.getGattServer();
+
+        isProvidingAndAdvertisingServices.setValue(false);
+        isAdvertisingService.setValue(false);
+
         /* start advertising & build observable chains
          *
          * setForeground?
          */
-
 
         return mBinder;
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
+        fidoAuthServiceDisposable.dispose();
         // TODO: stop observable chain with onComplete()
         return false;
     }
@@ -47,5 +79,75 @@ public class FidoAuthService extends Service {
 
     private boolean bleEnabled() {
         return ((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE)).getAdapter().isEnabled();
+    }
+
+    public LiveData<Boolean> isProvidingAndAdvertisingServices() {
+        return isProvidingAndAdvertisingServices;
+    }
+
+    public void toggleProvidingAndAdvertisingServices() {
+        if (!isProvidingAndAdvertisingServices.getValue()) {
+            startProvidingAndAdvertisingServices();
+        } else {
+            stopProvidingAndAdvertisingServices();
+        }
+    }
+
+    private void startProvidingAndAdvertisingServices() {
+        Timber.d("Starting to provide services");
+        provideAndAdvertiseServicesDisposable = bleServer.provideServices()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(disposable -> isProvidingAndAdvertisingServices.postValue(true))
+                .doFinally(() -> isProvidingAndAdvertisingServices.postValue(false))
+                .subscribe(
+                        () -> Timber.i("Stopped providing services"),
+                        this::postError
+                );
+        /*updateValueDisposable = fidoGattProfile.updateCharacteristicValues()
+                .subscribeOn(Schedulers.io())
+                .subscribe(
+                        () -> Timber.d("Done updating characteristic values"),
+                        this::postError
+                );*/
+        Timber.d("Starting to advertise services");
+        UUID uuid = FidoGattProfile.FIDO_SERVICE_UUID;
+        advertiseServicesDisposable = bleServer.advertise(uuid)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(disposable -> isAdvertisingService.postValue(true))
+                .doFinally(() -> isAdvertisingService.postValue(false))
+                .subscribe(
+                        () -> Timber.i("Stopped advertising services"),
+                        this::postError
+                );
+
+
+        fidoAuthServiceDisposable.add(provideAndAdvertiseServicesDisposable);
+        //viewModelDisposable.add(updateValueDisposable);
+        fidoAuthServiceDisposable.add(advertiseServicesDisposable);
+    }
+
+    private void stopProvidingAndAdvertisingServices() {
+        Timber.d("Stopping to advertise services");
+        if (advertiseServicesDisposable != null && !advertiseServicesDisposable.isDisposed()) {
+            advertiseServicesDisposable.dispose();
+        }
+        Timber.d("Stopping to provide services");
+        if (provideAndAdvertiseServicesDisposable != null && !provideAndAdvertiseServicesDisposable.isDisposed()) {
+            provideAndAdvertiseServicesDisposable.dispose();
+        }
+        /*if (updateValueDisposable != null && !updateValueDisposable.isDisposed()) {
+            updateValueDisposable.dispose();
+        }*/
+    }
+
+    private void postError(@NonNull Throwable throwable) {
+        Timber.w(throwable);
+        errors.postValue(throwable);
+    }
+
+    public LiveData<Throwable> getErrors() {
+        return errors;
     }
 }
--- app/src/main/java/com/blue_unicorn/android_auth/MainActivity.java
+++ app/src/main/java/com/blue_unicorn/android_auth/MainActivity.java
@@ -3,11 +3,13 @@ package com.blue_unicorn.android_auth;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -15,9 +17,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.blue_unicorn.android_auth_lib.android.FidoAuthService;
+import com.blue_unicorn.android_auth_lib.android.FidoAuthServiceBinder;
 import com.google.android.material.snackbar.Snackbar;
 import com.tbruyelle.rxpermissions3.RxPermissions;
 
@@ -25,24 +27,45 @@ import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Don't attempt to unbind from the service unless the client has received some
-    // information about the service's state.
-    private boolean mShouldUnbind;
-
-    private ServiceConnection mConnection = new FidoAuthServiceConnection();
-
     private static final int REQUEST_ENABLE_BLUETOOTH = 1;
 
-    private ViewModel viewModel;
+    private boolean mBound;
+    private FidoAuthService fidoAuthService;
+
     private RxPermissions rxPermissions;
     private ConstraintLayout constraintLayout;
     private ToggleButton advertiseServicesToggleButton;
     private Snackbar errorSnackbar;
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            fidoAuthService = ((FidoAuthServiceBinder) service).getService();
+            mBound = true;
+
+            fidoAuthService.isProvidingAndAdvertisingServices().observe(MainActivity.this, isProvidingAndAdvertisingService -> {
+                if (isProvidingAndAdvertisingService) {
+                    onServiceAdvertisingStarted();
+                } else {
+                    onServiceAdvertisingStopped();
+                }
+            });
+
+            fidoAuthService.getErrors().observe(MainActivity.this, throwable -> {
+                showTemporaryMessage(throwable);
+                performTroubleshooting(throwable);
+            });
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            fidoAuthService = null;
+            mBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Timber.d("START");
+
         setContentView(R.layout.activity_main);
 
         if (BuildConfig.DEBUG) {
@@ -50,35 +73,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         rxPermissions = new RxPermissions(this);
-        viewModel = new ViewModelProvider(this).get(ViewModel.class);
 
         constraintLayout = findViewById(R.id.coordinatorLayout);
         advertiseServicesToggleButton = findViewById(R.id.advertiseServicesToggleButton);
-        advertiseServicesToggleButton.setOnClickListener(v -> viewModel.toggleAdvertisingServices());
+        advertiseServicesToggleButton.setOnClickListener(v -> fidoAuthService.toggleProvidingAndAdvertisingServices());
 
         errorSnackbar = Snackbar.make(constraintLayout, R.string.error_unknown, Snackbar.LENGTH_SHORT);
 
-        // TODO: make this reactive
-        //bindToFidoAuthService();
-
-        viewModel.isAdvertisingService().observe(this, isAdvertisingService -> {
-            if (isAdvertisingService) {
-                onServiceAdvertisingStarted();
-            } else {
-                onServiceAdvertisingStopped();
-            }
-        });
-
-        viewModel.getErrors().observe(this, throwable -> {
-            showTemporaryMessage(throwable);
-            performTroubleshooting(throwable);
-        });
+        Intent intent = new Intent(this, FidoAuthService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //unbindFromFidoAuthService();
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
     }
 
     private void onServiceAdvertisingStarted() {
@@ -88,34 +100,6 @@ public class MainActivity extends AppCompatActivity {
     private void onServiceAdvertisingStopped() {
         advertiseServicesToggleButton.setChecked(false);
     }
-
-
-    // Fido Auth Service
-
-    private void bindToFidoAuthService() {
-        // Attempts to establish a connection with the service.  We use an
-        // explicit class name because we want a specific service
-        // implementation that we know will be running in our own process
-        // (and thus won't be supporting component replacement by other
-        // applications).
-        if (bindService(new Intent(MainActivity.this, FidoAuthService.class),
-                mConnection, Context.BIND_AUTO_CREATE)) {
-            mShouldUnbind = true;
-        } else {
-            Timber.e("Error: The requested service doesn't exist, or this client isn't allowed access to it.");
-        }
-    }
-
-    private void unbindFromFidoAuthService() {
-        if (mShouldUnbind) {
-            // Release information about the service's state.
-            unbindService(mConnection);
-            mShouldUnbind = false;
-        }
-    }
-
-
-    // Permissions
 
     private void checkPermissions() {
         int bluetoothPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH);
@@ -142,9 +126,6 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-
-    // Bluetooth
-
     private void checkBluetoothEnabled() {
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
@@ -163,9 +144,6 @@ public class MainActivity extends AppCompatActivity {
         Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
         startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
     }
-
-
-    // Util
 
     private void performTroubleshooting(@NonNull Throwable throwable) {
         checkPermissions();
