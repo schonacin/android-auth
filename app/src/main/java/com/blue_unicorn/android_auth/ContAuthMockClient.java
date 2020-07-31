eccde1e17b481078d6029ed78aed58af5bacc751
x3000621 <niklas.dornick@student.hpi.uni-potsdam.de>
2020-07-31 18:47:42 +0200

    mock client stops after first failed background auth

--- app/src/main/java/com/blue_unicorn/android_auth/ContAuthMockClient.java
+++ app/src/main/java/com/blue_unicorn/android_auth/ContAuthMockClient.java
@@ -27,7 +27,7 @@ public class ContAuthMockClient {
             if (sendGetAssertionContinuously) {
                 fidoAuthService.getAuthHandler().getApiLayer().buildNewRequestChain(RAW_GET_ASSERTION_REQUEST_WITH_EXTENSION_PARAMETER);
                 handler.postDelayed(this, interval);
-            }
+            } else return;
         }
     };
 
@@ -85,6 +85,7 @@ public class ContAuthMockClient {
                     }
                 } else {
                     Timber.d("RESPONSE CONTAINED ERROR");
+                    sendGetAssertionContinuously = false;
                 }
             }
 
